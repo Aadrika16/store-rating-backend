4888e0292218c77caef87a4090456608d08b2ef0
Aadrika16 <madhurinemuri5@gmail.com>
2025-07-12 20:38:33 +0530

    Update node.js

--- node.js
+++ node.js
@@ -14,6 +14,10 @@ app.use(cors({
   origin: 'https://store-rating-fullstack-app.netlify.app',
   credentials: true,
 }));
+app.options('*', cors({
+  origin: 'https://store-rating-fullstack-app.netlify.app',
+  credentials: true,
+}));
 app.use(express.json());
 
 let db = null;
